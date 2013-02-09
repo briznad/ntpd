378d490e8d5a25cb49e2fa868c8700a93fec0350
Brad Mallow <briznad@gmail.com>
2013-02-09 10:52:36 -0800

    changed wait interval from 1000 to 750ms

--- js/do.js
+++ js/do.js
@@ -1,3 +1,19 @@
-function ntpDo () {
-  window.location = window.location.href.split('auto' + 'focus>')[0] + 'auto' + 'focus>' + document.getElementById('ntpd').value + '<\/textarea>';
-}
+var t = null,
+
+	wait = function() {
+
+		t = setTimeout(function () {
+
+			window.location = window.location.href.split("auto" + "focus>")[0] + "auto" + "focus>" + document.getElementById("ntpd").value + "</textarea>";
+
+		}, 750);
+
+	},
+
+	ntpDo = function () {
+
+		clearTimeout(t);
+
+		wait();
+
+	};
